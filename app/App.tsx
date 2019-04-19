366f30a0a23ec575b03998e2b51a66713f4d4d60
johnwu <jwu5095@gmail.com>
2019-04-19 13:22:39 -0400

    Upgraded to react-native 0.59.3

--- app/App.tsx
+++ app/App.tsx
@@ -1,15 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * 
- * Generated with the TypeScript template
- * https://github.com/emin93/react-native-template-typescript
- * 
- * @format
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,7 +8,7 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-interface Props {}
+interface Props { }
 export default class App extends Component<Props> {
   render() {
     return (
